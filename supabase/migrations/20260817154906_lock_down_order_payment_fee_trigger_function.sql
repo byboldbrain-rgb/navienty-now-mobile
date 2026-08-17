f07cbf7ced22d3916d5465d81a69dc43b9481bce
Mahmoud <byboldbrain@gmail.com>
2026-08-17 18:51:37 +0300

    Track payment fee trigger hardening

---
+++ supabase/migrations/20260817154906_lock_down_order_payment_fee_trigger_function.sql
@@ -0,0 +1,3 @@
+revoke all on function now.apply_order_payment_fee() from public;
+revoke all on function now.apply_order_payment_fee() from anon;
+revoke all on function now.apply_order_payment_fee() from authenticated;
