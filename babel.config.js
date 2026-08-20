81323bdd02591fb11e6d0cd8d3115aeaeb67054b
Mahmoud <byboldbrain@gmail.com>
2026-08-20 06:17:15 +0300

    Resolve Expo Babel preset from installed Expo package

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,21 @@
+const path = require('node:path');
+
+const expoPackageDirectory = path.dirname(
+  require.resolve('expo/package.json'),
+);
+
+const babelPresetExpo = require.resolve(
+  'babel-preset-expo',
+  {
+    paths: [expoPackageDirectory],
+  },
+);
+
 module.exports = function (api) {
   api.cache(true);
 
   return {
-    presets: ['babel-preset-expo'],
+    presets: [babelPresetExpo],
     plugins: [
       './scripts/babel-plugin-navienty-cached-image.cjs',
     ],
